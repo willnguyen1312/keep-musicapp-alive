f46903ea2c71e68620e6f2a0e659d079c132882a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-02-06 19:50:49 +0700

    FIx typo

--- src/keepAlive.ts
+++ src/keepAlive.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const IS_PRO = process.env.NODE_ENV === 'production';
 
 const url = IS_PRO
-? 'https://personal-music-app-api.herokuapp.com/api/hello';
-: 'http://localhost:3333/api/hello'
+  ? 'https://personal-music-app-api.herokuapp.com/api/hello'
+  : 'http://localhost:3333/api/hello';
 
 setInterval(() => {
   axios.get(url).then((res) => console.log(res.data));
